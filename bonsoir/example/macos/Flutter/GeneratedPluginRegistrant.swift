909bc54a56ce7156df6110a3ad6f3951971c93ba
Hugo Delaunay <Skyost@users.noreply.github.com>
2023-06-15 15:56:36 +0200

    Updated the example app (see #52).

--- bonsoir/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ bonsoir/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import bonsoir_darwin
+import device_info_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SwiftBonsoirPlugin.register(with: registry.registrar(forPlugin: "SwiftBonsoirPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
 }
