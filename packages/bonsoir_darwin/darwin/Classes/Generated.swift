80609c4ba85487fdd0a3f8c932ef5ffb1c6aaef6
Hugo Delaunay <Skyost@users.noreply.github.com>
2023-11-30 14:23:30 +0100

    chore: Updated `melos` and `Generated` files.

--- packages/bonsoir_darwin/darwin/Classes/Generated.swift
+++ packages/bonsoir_darwin/darwin/Classes/Generated.swift
@@ -4,7 +4,7 @@ class Generated {
   /// Contains the broadcast messages.
   static let broadcastMessages: [String: String] = ["broadcastStarted": "Bonsoir service broadcast started : %s.", "broadcastNameAlreadyExists": "Trying to broadcast a service with a name that already exists : %s (old name was %s).", "broadcastStopped": "Bonsoir service broadcast stopped : %s.", "broadcastInitialized": "Bonsoir service broadcast initialized : %s.", "broadcastError": "Bonsoir service failed to broadcast : %s (error : %s)."]
 
-    /// broadcastStarted
+  /// broadcastStarted
   static let broadcastStarted: String = "broadcastStarted"
   /// broadcastNameAlreadyExists
   static let broadcastNameAlreadyExists: String = "broadcastNameAlreadyExists"
@@ -19,7 +19,7 @@ class Generated {
   /// Contains the discovery messages.
   static let discoveryMessages: [String: String] = ["discoveryStarted": "Bonsoir discovery started : %s.", "discoveryServiceFound": "Bonsoir has found a service : %s.", "discoveryServiceResolved": "Bonsoir has resolved a service : %s.", "discoveryServiceResolveFailed": "Bonsoir has failed to resolve a service : %s (error : %s).", "discoveryServiceLost": "A Bonsoir service has been lost : %s.", "discoveryStopped": "Bonsoir discovery stopped : %s.", "discoveryUndiscoveredServiceResolveFailed": "Trying to resolve an undiscovered service : %s of type %s.", "discoveryTxtResolved": "Bonsoir has found the attributes of a service : %s (new attributes are : %s).", "discoveryTxtResolveFailed": "Bonsoir has failed to get the TXT record of a service : %s (error %s).", "discoveryError": "Bonsoir has encountered an error during discovery : %s (error %s)."]
 
-    /// discoveryStarted
+  /// discoveryStarted
   static let discoveryStarted: String = "discoveryStarted"
   /// discoveryServiceFound
   static let discoveryServiceFound: String = "discoveryServiceFound"
