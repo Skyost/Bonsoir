acc09e736b325f3e77efc8293f603810b9259d52
Hugo Delaunay <Skyost@users.noreply.github.com>
2024-01-04 17:46:59 +0100

    chore(example): Added some links to the `main.dart` header.
    Closes #77.

--- packages/bonsoir/example/lib/main.dart
+++ packages/bonsoir/example/lib/main.dart
@@ -6,6 +6,13 @@ import 'package:bonsoir_example/widgets/eager_initialization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// This file is the entry point of the Bonsoir example project.
+/// The full source code is available here :
+/// https://github.com/Skyost/Bonsoir/tree/master/packages/bonsoir/example.
+///
+/// Feel free to check the available code snippets as well :
+/// https://bonsoir.skyost.eu/docs#getting-started
+
 /// Plugin's main method.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
